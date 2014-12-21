a2a01741e939b8d090b99c0b8e6343cd384d6180
meszarosotto <meszaros.otto@gmail.com>
2014-12-21 23:19:38 +0100

    # Please enter the commit message for your changes. Lines starting 	# with '#' will be ignored, and an empty message aborts the commit. 	# On branch master 	# Your branch is up-to-date with 'origin/master'. 	# 	# Changes to be committed: 	#	modified:   cachematrix.R 	#

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates an object with methods for implement cache  
 
-makeCacheMatrix <- function(x = matrix()) {
+  makeCacheMatrix <- function(x = matrix()) {
+    ## intialize the inverse matrix
+    inver <- NULL
+    ## set and get methods
+    set <- function(a_matrix) {
+      x <<- a_matrix
+      inver <<- NULL
+    }
+    get <- function() x
+    # set/getinverse methods
+    setinverse <- function(a_inver) inver <<- a_inver
+    getinverse <- function() inver
+    
+    # list of matrix elements
+    list(set = set, get = get,
+         setinverse = setinverse, 
+         getinverse = getinverse)
+  }
 
-}
-
-
-## Write a short comment describing this function
+## the cacheSolve function calculates inverse matrix of any matrix which has a solution 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  i_matrix<-x$getinverse()
+  ## check if the matrix's inverse already calculated or not
+  if(!is.null(i_matrix)) {
+    message("getting cached data")
+    return(i_matrix)
+  }
+  data<-x$get()
+  ## see determinant to check if solution exist or not
+    if(det(data)!=0){
+    i_matrix<-solve(data, ...)
+    x$setinverse(i_matrix)
+    return(i_matrix)
+  }
+  message("matrix has no inverse")
 }
